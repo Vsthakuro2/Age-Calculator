4dad2587d3e635f98057abcc32065ae862234f97
Vsthakuro2 <143859716+Vsthakuro2@users.noreply.github.com>
2023-10-17 16:53:09 +0530

    Add files via upload

---
+++ age.js
@@ -0,0 +1,19 @@
+function calculateAge(){
+    const dob=new Date(
+        document.getElementById('dob').value);
+    const today=new Date()
+ 
+ if(isNaN(dob.getTime())){
+    document.getElementById('output').innerHTML="Please Enter the valid date.";
+    return;
+
+ }
+ let age=today.getFullYear() - dob.getFullYear();
+ const monthDifference=today.getMonth()-dob.getMonth();
+ 
+ if(monthDifference < 0 ||(monthDifference === 0 && today.getDate() < dob.getDate())){
+    age--;
+
+ }
+ document.getElementById('output').innerHTML= "you are "+age +" years old";
+}
